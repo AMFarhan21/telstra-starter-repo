92589b1599e7ad2ae0f4848e4d251643100e4caa
AMFarhan <AMFarhan2102@gmail.com>
2025-12-31 13:59:50 +0800

    chore: remove unused import

--- src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
+++ src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
@@ -5,7 +5,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.springframework.beans.factory.annotation.Autowired;
